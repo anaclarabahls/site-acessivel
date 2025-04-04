c20a3820cea98d4837f86730cb415a66e16afb03
anaclarabahls <160857705+anaclarabahls@users.noreply.github.com>
2025-04-04 19:52:36 -0300

    .

--- script2.js
+++ script2.js
@@ -15,13 +15,12 @@ document.addEventListener('DOMContentLoaded', function(){
 
     aumentaFonteBotao.addEventListener('click', function(){
         tamanhoAtualFonte += 0.1;
-        document.body.style.fontSize = ${tamanhoAtualFonte}rem
+        document.body.style.fontSize = $;{tamanhoAtualFonte}rem
     })
 
     diminuiFonteBotao.addEventListener('click', function(){
         tamanhoAtualFonte -= 0.1;
-        document.body.style.fontSize = ${tamanhoAtualFonte}rem
+        document.body.style.fontSize = $;{tamanhoAtualFonte}rem
 
     })
     })
-    
